5f7c61cff8039c2d3bc9dabf208336d421fbd47d
KrLite <68179735+KrLite@users.noreply.github.com>
2024-04-08 20:50:05 +0800

    Initial Commit

---
+++ Clipchop/ClipchopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipchopApp.swift
+//  Clipchop
+//
+//  Created by KrLite on 2024/4/8.
+//
+
+import SwiftUI
+
+@main
+struct ClipchopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
