cd390d15d3c38bb4f5de4e93b4ff663afbdb2f74
KrLite <68179735+KrLite@users.noreply.github.com>
2024-05-12 17:57:52 +0800

    icons, permissions

--- Clipchop/Utilities/PerimissionsManager.swift
+++ Clipchop/Utilities/PerimissionsManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import AppKit
+import FullDiskAccess
 
 class PermissionsManager {
     static func requestAccess() {
@@ -15,7 +16,7 @@ class PermissionsManager {
     
     class Accessibility {
         static func getStatus() -> Bool {
-            // Get current state for accessibility access
+            // Get current status for Accessibility Access
             let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString: false]
             let status = AXIsProcessTrustedWithOptions(options)
             
@@ -25,14 +26,14 @@ class PermissionsManager {
         @discardableResult
         static func requestAccess() -> Bool {
             // More information on this behaviour: https://stackoverflow.com/questions/29006379/accessibility-permissions-reset-after-application-update
-            guard !PermissionsManager.Accessibility.getStatus()  else { return true }
+            guard !Accessibility.getStatus() else { return true }
             
             let alert = NSAlert()
             alert.alertStyle = NSAlert.Style.informational
             alert.messageText = String(
                 format: String(localized: .init(
                     "Accessibility Access Alert: Title",
-                    defaultValue: "%@ needs Accessibility Access"
+                    defaultValue: "%@ Needs Accessibility Access"
                 )),
                 Bundle.main.appName
             )
@@ -40,10 +41,10 @@ class PermissionsManager {
                 format: String(localized: .init(
                     "Accessibility Access Alert: Content",
                     defaultValue: """
-Accessibility Access is required for \(Bundle.main.appName) to take over your clipboard. No data will be collected remotely.
+Accessibility Access is required for %@ to take over your clipboard. No data will be collected remotely.
 """
                 )),
-                Bundle.main.appName, Bundle.main.appName
+                Bundle.main.appName
             )
             
             alert.runModal()
@@ -52,6 +53,43 @@ Accessibility Access is required for \(Bundle.main.appName) to take over your cl
             let status = AXIsProcessTrustedWithOptions(options)
             
             return status
+        }
+    }
+    
+    class FullDisk {
+        static func getStatus() -> Bool {
+            // Get current status for Full Disk Access
+            return FullDiskAccess.isGranted
+        }
+        
+        @discardableResult
+        static func requestAccess() -> Bool {
+            guard !FullDisk.getStatus() else { return true }
+            
+            FullDiskAccess.promptIfNotGranted(
+                title: String(
+                    format: String(localized: .init(
+                        "Full Disk Access Alert: Title",
+                        defaultValue: "%@ Needs Full Disk Access"
+                    )),
+                    Bundle.main.appName
+                ),
+                message: String(
+                    format: String(localized: .init(
+                        "Full Disk Access Alert: Content",
+                        defaultValue: """
+Full Disk Access is required for %@ to generate file previews. No data will be collected remotely.
+"""
+                    )),
+                    Bundle.main.appName
+                ),
+                settingsButtonTitle: String(localized: .init("Open Settings", defaultValue: "Open Settings")),
+                skipButtonTitle: String(localized: .init("Later", defaultValue: "Later")),
+                canBeSuppressed: false,
+                icon: nil
+            )
+            
+            return getStatus()
         }
     }
 }
