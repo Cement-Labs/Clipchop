3ec34910030b32e9b47170c27a0aa9c4dc2b9c13
KrLite <68179735+KrLite@users.noreply.github.com>
2024-05-13 22:53:58 +0800

    more clipboard & history settings

--- Clipchop/Settings/Sections/Clipboard/ClipboardHistorySection.swift
+++ Clipchop/Settings/Sections/Clipboard/ClipboardHistorySection.swift
@@ -11,34 +11,85 @@ import Defaults
 struct ClipboardHistorySection: View {
     @Default(.historyPreservationPeriod) var historyPreservationPeriod
     @Default(.historyPreservationTime) var historyPreservationTime
+    @Default(.timerInterval) var timerInterval
+    
+    @State var isDeleteHistoryAlertPresented = false
+    
+    @ViewBuilder
+    func periodWithTime(_ time: Int, period: HistoryPreservationPeriod) -> some View {
+        switch period {
+        case .forever: Text("Forever")
+        case .minute:
+            Text("\(time) Minute")
+        case .hour:
+            Text("\(time) Hour")
+        case .day:
+            Text("\(time) Day")
+        case .month:
+            Text("\(time) Month")
+        case .year:
+            Text("\(time) Year")
+        }
+    }
     
     var body: some View {
         Section("Clipboard History") {
             VStack {
                 Picker("Preservation time", selection: $historyPreservationPeriod) {
                     ForEach(HistoryPreservationPeriod.allCases) { period in
-                        let time = Int(historyPreservationTime)
-                        
-                        switch period {
-                        case .forever: Text("Forever")
-                        case .minute:
-                            Text("\(time) Minute")
-                        case .hour:
-                            Text("\(time) Hour")
-                        case .day:
-                            Text("\(time) Day")
-                        case .month:
-                            Text("\(time) Month")
-                        case .year:
-                            Text("\(time) Year")
-                        }
+                        periodWithTime(Int(historyPreservationTime), period: period)
                     }
                 }
                 
-                Slider(value: $historyPreservationTime, in: 1...30, step: 1)
-                    .disabled(historyPreservationPeriod == .forever)
+                Slider(value: $historyPreservationTime, in: 1...30, step: 1) {
+                    
+                } minimumValueLabel: {
+                    Text("1")
+                } maximumValueLabel: {
+                    Text("30")
+                }
+                .monospaced()
+                .disabled(historyPreservationPeriod == .forever)
+            }
+            
+            VStack {
+                HStack {
+                    Text("Update interval")
+                    Spacer()
+                    Text("\(timerInterval, specifier: "%.2f")s")
+                        .monospaced()
+                }
+                
+                Slider(value: $timerInterval, in: 0.1...5) {
+                    
+                } minimumValueLabel: {
+                    Text("0.10")
+                } maximumValueLabel: {
+                    Text("5.00")
+                }
+                .monospaced()
             }
         }
+        
+        Section {
+            Button {
+                isDeleteHistoryAlertPresented = true
+            } label: {
+                Text("Clear clipboard history")
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.plain)
+            .controlSize(.large)
+            .alert("Clear Clipboard History", isPresented: $isDeleteHistoryAlertPresented) {
+                Button("Delete", role: .destructive) {
+                    
+                }
+            } message: {
+                Text("This action clears all your clipboard history unrestorably, including pins.")
+            }
+            .padding(-5)
+        }
+        .foregroundStyle(.red)
     }
 }
 
