397ec9a68747f3411f724e5157ea8fcc881655c4
KrLite <68179735+KrLite@users.noreply.github.com>
2024-05-18 15:25:38 +0800

    creation

---
+++ Clipchop/Clipboard/SwiftData/ClipboardContent.swift
@@ -0,0 +1,22 @@
+//
+//  ClipboardContent.swift
+//  Clipchop
+//
+//  Created by KrLite on 2024/5/18.
+//
+//
+
+import Foundation
+import SwiftData
+
+
+@Model class ClipboardContent {
+    var type: String?
+    var value: Data?
+    
+    @Relationship(inverse: \ClipboardHistory.contents) var item: ClipboardHistory?
+    
+    public init() {
+        
+    }
+}
---
+++ Clipchop/Clipboard/SwiftData/ClipboardHistory.swift
@@ -0,0 +1,104 @@
+//
+//  ClipboardHistory.swift
+//  Clipchop
+//
+//  Created by KrLite on 2024/5/18.
+//
+//
+
+import Foundation
+import SwiftData
+import AppKit
+
+
+@Model class ClipboardHistory {
+    enum Class {
+        case all
+        
+        case file
+        case html
+        case image
+        case rtf
+        case text
+        
+        var types: [NSPasteboard.PasteboardType] {
+            switch self {
+            case .all: [
+                .rtf, .rtfd,
+                .html,
+                .string,
+                .fileURL, .URL,
+                .jpeg, .tiff, .png,
+                .pdf,
+                .universalClipboard,
+                .tabularText,
+                .multipleTextSelection,
+                .fileContents
+            ]
+            case .file: [
+                .fileURL
+            ]
+            case .html: [
+                .html
+            ]
+            case .image: [
+                .jpeg, .tiff, .png
+            ]
+            case .rtf: [
+                .rtf, .rtfd
+            ]
+            case .text: [
+                .string
+            ]
+            }
+        }
+        
+        var typeStrings: [String] {
+            types.map { $0.rawValue }
+        }
+    }
+    
+    static var source: NSRunningApplication? {
+        NSWorkspace.shared.frontmostApplication
+    }
+    
+    static var pasteboard: NSPasteboard {
+        .general
+    }
+    
+    // MARK: - Fields
+    
+    var app: String?
+    @Attribute(.unique)
+    var id: UUID
+    var pinned: Bool
+    var time: Date?
+    var title: String?
+    var contents: ClipboardContent?
+    
+    public init() {
+        self.id = .init()
+        self.app = ClipboardHistory.source?.localizedName
+        self.time = Date.now
+        self.title = formatter.title
+        self.pinned = false
+        
+        contents.forEach(addToContents(_:))
+    }
+    
+    // MARK: - Functions
+    
+    func getContents() -> [ClipboardContent] {
+        (contents?.allObjects as? [ClipboardContent]) ?? []
+    }
+    
+    func supersedes(_ item: ClipboardHistory) -> Bool {
+        item.getContents()
+            .filter { content in
+                content.type == NSPasteboard.PasteboardType.fromClipchop.rawValue
+            }
+            .allSatisfy { content in
+                getContents().contains([content])
+            }
+    }
+}
