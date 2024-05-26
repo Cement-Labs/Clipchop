67cfd58bf97339c47be40b25a2f4988325d9a63f
KrLite <68179735+KrLite@users.noreply.github.com>
2024-05-26 16:21:17 +0800

    this works.

--- Clipchop/Views/Settings/Sections/Categories/FileTypeListView.swift
+++ Clipchop/Views/Settings/Sections/Categories/FileTypeListView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import WrappingHStack
 
 struct FileTypeListView<Label>: View where Label: View {
     @ViewBuilder var label: () -> Label
@@ -34,12 +35,12 @@ struct FileTypeListView<Label>: View where Label: View {
     
     var body: some View {
         Section {
-            VStack {
-                GeometryReader { geometry in
-                    generateContent(in: geometry)
-                }
+            WrappingHStack(types, id: \.self) { type in
+                FileTypeTagView(type: type)
+                    .onDrag {
+                        NSItemProvider(object: type as NSString)
+                    }
             }
-            .frame(height: totalHeight)
         } header: {
             HStack {
                 label()
@@ -57,68 +58,6 @@ struct FileTypeListView<Label>: View where Label: View {
                 .buttonStyle(.borderless)
                 .buttonBorderShape(.circle)
             }
-        }
-    }
-    
-    // Originally by https://stackoverflow.com/a/65453108/23452915
-    @ViewBuilder
-    private func generateContent(in geometry: GeometryProxy) -> some View {
-        var width: CGFloat = .zero
-        var height: CGFloat = .zero
-        
-        ZStack(alignment: .topLeading) {
-            ForEach(types, id: \.self) { type in
-                FileTypeTagView(type: type)
-                    .onDrag {
-                        NSItemProvider(object: type as NSString)
-                    }
-                
-                    .alignmentGuide(.leading) { dimensions in
-                        if abs(width - dimensions.width) > geometry.size.width {
-                            width = 0
-                            height -= dimensions.height
-                        }
-                        
-                        let result = width
-                        if type == types.last {
-                            // The last item
-                            width = 0
-                        } else {
-                            width -= dimensions.width
-                        }
-                        
-                        return result
-                    }
-                    .alignmentGuide(.top) { dimensions in
-                        let result = height
-                        if type == types.last {
-                            // The last item
-                            height = 0
-                        }
-                        
-                        return result
-                    }
-                
-                    .if(isCollapsed && height < 0) { view in
-                        view.hidden()
-                    }
-            }
-        }
-        .background {
-            viewHeightReader($totalHeight)
-        }
-    }
-    
-    @ViewBuilder
-    private func viewHeightReader(_ totalHeight: Binding<CGFloat>) -> some View {
-        GeometryReader { geometry -> Color in
-            let rect = geometry.frame(in: .local)
-            
-            DispatchQueue.main.async {
-                totalHeight.wrappedValue = rect.size.height
-            }
-            
-            return .clear
         }
     }
 }
