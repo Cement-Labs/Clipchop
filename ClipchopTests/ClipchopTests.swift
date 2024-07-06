a087fc89b5094abdd352ef67950211f1e1e7dc61
KrLite <68179735+KrLite@users.noreply.github.com>
2024-07-06 17:14:34 +0800

    Implement unit testing schemes

---
+++ ClipchopTests/ClipchopTests.swift
@@ -0,0 +1,14 @@
+//
+//  ClipchopTests.swift
+//  ClipchopTests
+//
+//  Created by KrLite on 2024/7/6.
+//
+
+import Testing
+
+struct ClipchopTests {
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
